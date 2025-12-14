ee33093226ee086969ec3ed0adf85bc972b6fb5c
Charlie Thomson <charlie@thmsn.dev>
2025-12-13 20:24:17 -0500

    +logs

--- libpath/src/lib.rs
+++ libpath/src/lib.rs
@@ -1,7 +1,7 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, time::SystemTime};
 
-use libproduct::{ProductName, product_name};
-use tracing::{Level, instrument};
+use libproduct::{product_name, ProductName};
+use tracing::{instrument, Level};
 
 product_name!(with base "dev.thmsn.unspecified" as DEFAULT_PRODUCT_NAME);
 
@@ -19,13 +19,21 @@ impl MkdirIfNotExists for PathBuf {
 }
 
 #[must_use]
+fn epoch() -> u64 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Why are you in the past?")
+        .as_secs()
+}
+
+#[must_use]
 #[instrument(level = Level::DEBUG, ret)]
 /// # Panics
 ///
 /// Panics if `dirs` is unable to locate a [data_local_dir](https://docs.rs/dirs/latest/dirs/fn.data_local_dir.html)
 pub fn data_root() -> PathBuf {
-    let product_name = ProductName::global()
-        .map_or(DEFAULT_PRODUCT_NAME.to_string(), |name| name.to_string());
+    let product_name =
+        ProductName::global().map_or(DEFAULT_PRODUCT_NAME.to_string(), |name| name.to_string());
     dirs::data_local_dir()
         .expect("cant find data local dir")
         .join(&product_name)
@@ -44,4 +52,18 @@ pub fn config_path(module: &str) -> PathBuf {
     configs_root()
         .mkdir_if_not_exists()
         .join(format!("{module}.toml"))
+}
+
+#[must_use]
+#[instrument(level = Level::DEBUG, ret)]
+pub fn logs_root() -> PathBuf {
+    data_root().join("logs").mkdir_if_not_exists()
+}
+
+#[must_use]
+#[instrument(level = Level::DEBUG, ret)]
+pub fn log_path() -> PathBuf {
+    logs_root()
+        .mkdir_if_not_exists()
+        .join(format!("log_{}.json", epoch()))
 }
